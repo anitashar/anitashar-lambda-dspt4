7bfd056c4016f089116ededbbc07207d7480ecb2
anitashar <anitashar2006@gmail.com>
2020-03-11 23:13:11 -0700

    updated

--- my_lambdata/IQR.py
+++ my_lambdata/IQR.py
@@ -7,14 +7,22 @@ import numpy as np
 
 
 def removeOutliers(x):
-	   a = np.array(x)
-	   upper_quartile = np.percentile(a, 75)
-	   lower_quartile = np.percentile(a, 25)
-	   IQR = (upper_quartile - lower_quartile) * 1.5
-	   quartileSet = (lower_quartile - IQR, upper_quartile + IQR)
-	   resultList = []
-	   for y in a.tolist():
-        	if y >= quartileSet[0] and y <= quartileSet[1]: 
-        		resultList.append(y)
-        	return resultList   
+	a = np.array(x)
+	upper_quartile = np.percentile(a, 75)
+	lower_quartile = np.percentile(a, 25)
+	IQR = (upper_quartile - lower_quartile) * 1.5
+	quartileSet = (lower_quartile - IQR, upper_quartile + IQR)
+	resultList = []
+	
+	for y in np.nditer(a):
+		print(y)
+		
+		if y <= quartileSet[0] or y >= quartileSet[1]: 
+			resultList.append(y)
+	return resultList   
 	      
+
+if __name__ == "__main__":
+
+	print(removeOutliers([-10,2,5,3,8,4,7,5,10,99,1000]))
+
